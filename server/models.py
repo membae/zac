c1cc2af4b6283d4fa2e6d6af58f2ff1f839ca05a
memba <membaphilip1@gmail.com>
2025-01-26 22:50:09 +0300

    model for users done

---
+++ server/models.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Metadata
+metadata=Metadata()
+db=SQLAlchemy(metadata=metadata)
+
+class User(db.Model):
+    __tablename__='users'
+    
+    id=db.Column(db.Integer,primary_key=True)
+    email=db.Column(db.String,nullable=False)
+    password=db.Column(db.String,nullable=False)
+    
+    
